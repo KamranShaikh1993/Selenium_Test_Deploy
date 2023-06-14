bff5b138855187089cc604f4fbe0fee5a36ad910
Kamran Shaikh <109130535+KamranShaikh1993@users.noreply.github.com>
2023-06-14 11:14:08 +0530

    Update index.py

--- index.py
+++ index.py
@@ -57,7 +57,7 @@ def load_data():
     fname = os.path.join(d,'chromedriver.exe')
 
     # Load driver
-    driver=webdriver.chrome_driver(executable_path= 'KamranShaikh1993/Selenium_Test_Deploy/blob/main/chromedriver.exe')
+    driver=webdriver.Chrome(executable_path= 'KamranShaikh1993/Selenium_Test_Deploy/blob/main/chromedriver.exe')
     # geckodriver.exe 
     
     
